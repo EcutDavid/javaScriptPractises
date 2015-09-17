13f33289705ce7091be9e5c808f7cf93064a09e5
David <Ecutwxguan@163.com>
2015-09-17 19:57:51 +0800

    Add chart in html.

--- D3.js_DataDrivenDocument/scripts/entry.js
+++ D3.js_DataDrivenDocument/scripts/entry.js
@@ -1,5 +1,18 @@
 import d3 from 'd3';
+require('../style/chart.css');
 
-let body = d3.select("#app");
+//select element, like all querySelector
+
+//it can with select || selectAll
+let body = d3.selectAll("body");
 let div = body.append("div");
 div.html("Hello, world!!");
+
+let app = d3.select('#app');
+let title = app.append('h1');
+title.html('it works');
+
+//change style
+app.style('color', '#666')
+    .style('border', 'solid')
+    .style('text-align', 'center');
