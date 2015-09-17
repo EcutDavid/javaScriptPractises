f0068ae0b0b4ac0e3d3776a62defdce522dd8068
David <Ecutwxguan@163.com>
2015-09-17 20:16:08 +0800

    replace html with js.

--- D3.js_DataDrivenDocument/scripts/entry.js
+++ D3.js_DataDrivenDocument/scripts/entry.js
@@ -6,13 +6,28 @@ require('../style/chart.css');
 //it can with select || selectAll
 let body = d3.selectAll("body");
 let div = body.append("div");
-div.html("Hello, world!!");
+// div.html("Hello, world!!");
 
 let app = d3.select('#app');
 let title = app.append('h1');
 title.html('it works');
 
+d3.select('#sectionB').append('div')
+  .html('added from Channing style code');
+
+//The recommended indentation pattern for method chaining is four spaces for methods that preserve
+//the current selection and two spaces for methods that change the selection.
+
 //change style
 app.style('color', '#666')
     .style('border', 'solid')
     .style('text-align', 'center');
+
+//add chart in data
+let data = [43, 8, 15, 16, 23, 42];
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
